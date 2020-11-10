bc8e6cdd14ad6d224d2cb8cf9ca6fc6092509c7a
Anthony Wang <ta180m@gmail.com>
2020-11-10 20:01:52 +0000

    Testing

--- week1.py
+++ week1.py
@@ -1,20 +1,8 @@
-import this
+# Printing output
 
 print("hello world")
 
-def gcd(a, b):
-  if b > 0: return gcd(b, a % b)
-  else: return a
+print("2 + 2 = ")
+print(2 + 2)
 
-print("test")
 
-N = 1000
-A = []
-for i in range(0, N):
-  A.append(i)
-
-ans = 0
-for i in A:
-  for j in A:
-    ans += gcd(i, j)
-print(ans)
