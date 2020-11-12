2f1e810deee968f6eca76b5d723f9b022501e383
Anthony Wang <ta180m@gmail.com>
2020-11-12 16:20:05 +0000

    Fix case

--- week1.py
+++ week1.py
@@ -98,33 +98,33 @@ When you create a variable, you do 2 things
 '''
 
 
-# Let's make a variable named "ageOfBob." This value will represent the age of Bob :).
+# Let's make a variable named "age_of_Bob." This value will represent the age of Bob :).
 
-ageOfBob = 5
+age_of_Bob = 5
 # We're doing 2 things in 1 line
-# 1) We create a variable named "ageOfBob"
+# 1) We create a variable named "age_of_Bob"
 # 2) We give the variable the value 5.
 
-# ATTRIBUTES of the variable "ageOfBob"
-#      The variable NAME is "ageOfBob"
+# ATTRIBUTES of the variable "age_of_Bob"
+#      The variable NAME is "age_of_Bob"
 #      The variable's VALUE is 5
 #      The variable TYPE is Integer
 
 
-# Let's create a new variable named "ageOfSally." Sally is twice as old as Bob. 
-ageOfSally = ageOfBob * 2
+# Let's create a new variable named "age_of_Sally." Sally is twice as old as Bob. 
+age_of_Sally = age_of_Bob * 2
 # We are doing 3 things above:
-# 1) We are creating a new variable named "ageOfSally"
-# 2) We are calculating ageOfBob * 2 (which is =10)
-# 3) We are assigning 10 to the variable "ageOfSally"
-# Thus, ageOfSally now has a value of 10
+# 1) We are creating a new variable named "age_of_Sally"
+# 2) We are calculating age_of_Bob * 2 (which is =10)
+# 3) We are assigning 10 to the variable "age_of_Sally"
+# Thus, age_of_Sally now has a value of 10
 
 
 # It's Bob's birthday today!
 # He is now 6 years old
-# We can give the variable ageOfBob a new value
+# We can give the variable age_of_Bob a new value
 # After all, it's just a memory "slot"
-ageOfBob = 6
+age_of_Bob = 6
 
 
 '''
@@ -137,7 +137,7 @@ Then, print the value
 
 
 #  There are many types of variables
-# ageOfBob and ageOfSally are INTEGER type variables
+# age_of_Bob and age_of_Sally are INTEGER type variables
 # There are also String, arrays, and other types of variables
 
 name = "Bill Wender"
@@ -176,7 +176,7 @@ Figure out what is wrong in the code below!
 '''
 
 name = "Bob"
-newName = name + 1
+new_name = name + 1
 
 # Solution: the variables must be the SAME TYPE.
 # name is a string, while 1 is an integer
@@ -194,53 +194,53 @@ It's quite easy once we see an exampple
 # Let's get some info about the user
 print("What's your name?")
 
-yourName = input() # type your name into the console
+your_name = input() # type your name into the console
 
 print("Your name is:")
-print(yourName)
+print(your_name)
 
 
 print("What's your age?")
 
-yourAge = input()
+your_age = input()
 
 print("Your age is:")
-print(yourAge)
+print(your_age)
 
 print("After your next birthday, you will be:")
-print(yourAge + 1)
+print(your_age + 1)
 
 # Whoa!
 # input() gets input as a string
-# So, yourAge is a string
+# So, your_age is a string
 # And you can't add 1 to a string, as we saw earlier!
 
 # So, what do we do?
 print("Your age, as a string:")
-print(yourAge)
+print(your_age)
 
-# We can use int(yourAge) to CONVERT yourAge from a string into an integer
+# We can use int(your_age) to CONVERT your_age from a string into an integer
 print("Your age, as an integer:")
-print(int(yourAge))
+print(int(your_age))
 
 # Now it works!
 print("After your next birthday, you will be:")
-print(int(yourAge) + 1)
+print(int(your_age) + 1)
 
 
-firstName = "Bill"
-lastName = "Wender"
+first_name = "Bill"
+last_name = "Wender"
 
 # When we "add" two strings, we mash them together to create a new string
 # this called "concatenating" if you want to use the fancy word
-name = firstName + lastName
+name = first_name + last_name
 
 print("His name is:")
 print(name)
 
 # Oops! That doesn't look right.
 # Let's add a space in the middle
-name = firstName + " " + lastName
+name = first_name + " " + last_name
 print("This should be better:")
 print(name)
 
@@ -251,7 +251,8 @@ ACTIVITY 4: What's wrong with this code?
 Figure out what's wrong here!
 '''
 
-print("2 + 2 = " + 5)
+ans = 5
+print("2 + 2 = " + ans)
 
 
 
@@ -270,24 +271,42 @@ REVIEW
 Let's go over what we learned today
 '''
 
+# Variables have a NAME, a TYPE, and a VALUE
+
+# You can't mix and match different types when doing addition
+sum = 2 + 2 # OK
+full_name = "Bill" + " " + "Wender" # OK
+bad = "Bill" + 2 # ERROR
+
+
 # Below will be a bunch of lines of code. This is for those who learn better visually.
 
-myAge = 5
-myName = "Theo"
-myNewAge = myAge + 1
+my_age = 5
+my_name = "Theo"
+my_new_age = my_age + 1
 print("hi")
 
 # print("my name is")
-# print(myName)
-print("my name is: " + myName)
+# print(my_name)
+print("my name is: " + my_name)
 
 
 
 '''
-CHALLENGE TIME
+CHALLENGE 1: Mad Libs
 
 Want to go the extra mile? Try this challenge:
 
 
 '''
-# Mini-project here(maybe have them make a madlibs type thing?)
+
+
+
+'''
+CHALLENGE 2:
+
+Want to go the extra mile? Try this challenge:
+
+
+'''
+
