ced7a51cf7737b4b64b99d6c9833cb79932c91c8
Anthony Wang <ta180m@gmail.com>
2020-11-12 02:57:10 +0000

    Initial draft of week1.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,6 @@
 import random
 
+
 def print_board(myarray):  # function to display the tic-tac-toe board
     print("       Collumn")
     print("        0 1 2")
@@ -29,16 +30,20 @@ def print_board(myarray):  # function to display the tic-tac-toe board
     print("       ------- ")
 
 
-def player_input(board, row, collum, player):  # function to change a tile in the board
+def player_input(board, row, collum,
+                 player):  # function to change a tile in the board
     if board[row][collum] == 0:
-        board[row][collum] = player  # looks at a specific element in the board and changes the element to a plyer number
+        board[row][
+            collum] = player  # looks at a specific element in the board and changes the element to a plyer number
     else:
         print("That space is occupied!")
 
     return board  # returns the changed board
 
 
-def check_draw(board):  # returns False if game is a tie, return true if there is still open space
+def check_draw(
+        board
+):  # returns False if game is a tie, return true if there is still open space
     for row_number in range(3):
         for column_number in range(3):
             element = board[row_number][column_number]
@@ -56,7 +61,8 @@ def check_win(board):
 
     # check vertical lines
     for collumn_number in range(3):
-        if board[0][collumn_number] == board[1][collumn_number] == board[2][collumn_number] != 0:
+        if board[0][collumn_number] == board[1][collumn_number] == board[2][
+                collumn_number] != 0:
             return board[0][collumn_number]
 
     # check diagonals
@@ -75,7 +81,7 @@ def AI_check(board):
         for column in range(3):
             element = board[row][column]
             if element == 0:
-                empty_spaces.append([row,column])
+                empty_spaces.append([row, column])
 
     return empty_spaces
 
@@ -83,16 +89,22 @@ def AI_check(board):
 def AI_move(board, player):
     empty_spaces = AI_check(board)
     number_of_empty_spaces = len(empty_spaces)
-    rand = random.randint(0,number_of_empty_spaces-1)
+    rand = random.randint(0, number_of_empty_spaces - 1)
     return empty_spaces[rand]
 
 
 player_turn = 1
-board = [[0,0,0],  # we are using an array of arrays to store what the board looks like
-         [0,0,0],  # (and array that contains 3 arrays that contains 3 elements. This makes a 3x3 board)
-         [0,0,0]]
+board = [
+    [0, 0,
+     0],  # we are using an array of arrays to store what the board looks like
+    [
+        0, 0, 0
+    ],  # (and array that contains 3 arrays that contains 3 elements. This makes a 3x3 board)
+    [0, 0, 0]
+]
 
-print_board(board)  # we are calling the print_board to print what the board looks like
+print_board(
+    board)  # we are calling the print_board to print what the board looks like
 
 while True:  # this "while True" allows for code to repeat forever
     if_draw = check_draw(board)
@@ -103,15 +115,16 @@ while True:  # this "while True" allows for code to repeat forever
     print("Player " + str(player_turn) + " turn!")
 
     if player_turn == 1:
-        row = input("Which row do you want to place your piece?") # input is used to allow the user to enter data
+        row = input("Which row do you want to place your piece?"
+                    )  # input is used to allow the user to enter data
         column = input("Which column do you want to place you piece?")
     if player_turn == 2:
-        board_coordinate = AI_move(board,player_turn)
+        board_coordinate = AI_move(board, player_turn)
         row = board_coordinate[0]
         column = board_coordinate[1]
 
-
-    board = player_input(board,int(row),int(column), int(player_turn))  # we have to change row, column, and player to an int
+    board = player_input(board, int(row), int(column), int(
+        player_turn))  # we have to change row, column, and player to an int
     print_board(board)
 
     if_win = check_win(board)
--- week1.py
+++ week1.py
@@ -1,8 +1,219 @@
-# Printing output
+'''
+Introduction to Python
 
-print("hello world")
+Week 1
 
-print("2 + 2 = ")
+ - Printing output
+ 
+ - Variables
+ 
+ - Reading input
+
+'''
+
+
+'''
+Slides from last week:
+
+https://docs.google.com/presentation/d/1FP5xCCzeM-IY8dQ1ZJNYqjy3RSc3YqrJ267_a-5nUmE/edit?usp=sharing
+'''
+
+
+'''
+PRINTING OUTPUT
+'''
+
+# This is a comment
+# It doesn't do anything
+
+# A multiple-line comment
+'''
+Your first podcast will be awful
+Your first video will be awful
+Your first article will be awful
+Your first art will be awful
+Your first photo will be awful
+Your first game will be awful
+But your first code will be perfect.
+Zero bugs and very clean code.
+It will be "Hello, world!"
+'''
+
+# Your first program
+# Prints "Hello, world!" in the console
+# The console is the black box on the right
+# Press "Run" at the top to see it in action!
+print("Hello, world!")
+# The above line is a "command". It tells the computer to do something
+
+# Print some more text
+print("this is a string")
+
+# note that the 4 doesn't have quotes
+# it is an integer
+print(4)
+
+# Prints 4 in the "console"
 print(2 + 2)
 
+# Prints the LITERAL TEXT 2 + 2
+print("2 + 2")
 
+
+'''
+ACTIVITY 1: Print something fun!
+
+'''
+
+
+
+''''
+VARIABLES
+
+# Just like you, computers have memory
+# We use variables to store information in the computer's memory
+# A variable is just a named "slot" in the computer's memory
+
+# Imagine a computer's memory being a huge warehouse of unlabeled boxes
+# Each box represents a potential variable
+# When you create a variable, you do 2 things
+#  1) You write the variable NAME on the side of the box
+#  2) You put the variable's VALUE INTO the box
+# *all the unlabeled boxes are memory slots with no variable or value assigned
+'''
+
+# Let's make a variable named "ageOfBob." This value will represent the age of Bob :).
+
+ageOfBob = 5
+# We're doing 2 things in 1 line
+# 1) We create a variable named "ageOfBob"
+# 2) We give the variable the value 5.
+
+# ATTRIBUTES of the variable "ageOfBob"
+#      The variable NAME is "ageOfBob"
+#      The variable's VALUE is 5
+#      The variable TYPE is Integer
+
+
+# Let's create a new variable named "ageOfSally." Sally is twice as old as Bob. 
+ageOfSally = ageOfBob * 2
+# We are doing 3 things above:
+# 1) We are creating a new variable named "ageOfSally"
+# 2) We are calculating ageOfBob * 2 (which is =10)
+# 3) We are assigning 10 to the variable "ageOfSally"
+# Thus, ageOfSally now has a value of 10
+
+
+# It's Bob's birthday today!
+# He is now 6 years old
+# We can give the variable ageOfBob a new value
+# After all, it's just a memory "slot"
+ageOfBob = 6
+
+'''
+ACTIVITY 2
+Make a variable named "MyAge" and give it the value of your actual age
+Then, print the value 
+'''
+
+
+
+
+#  There are many types of variables
+# ageOfBob and ageOfSally are INTEGER type variables
+# There are also String, arrays, and other types of variables
+
+name = "Bill Wender"
+# The variable TYPE is String
+# When creating a String, make sure the value has quotations around it
+# Otherwise, the computer will think "Bill" and "Wender" are variable names
+
+# print the value of name. 
+print(name)
+
+# we can change the value of name
+name = "Bill Nye"
+
+# Now it's "Bill Nye"
+print(name)
+
+#How do you tell the different between INTEGER and STRING types?
+#Integers will be green
+#Strings will be orange with quotations
+
+print(5+5-(6*2)) # the computer will compute the value
+print("aos8d7fhw") # " " indicate it is a String
+print(ageOfBob) # without the " ", computer will assume the text is a variable
+
+
+'''
+ACTIVITY 3: WHAT IS WRONG WITH myVariable
+'''
+
+BobName = "Bob"
+# myVariable = Bob + ageOfBob
+# Solution: the variables must be the SAME TYPE.
+# It's like you're trying to add the number 7 to the word "tissue"
+
+
+'''
+READING INPUT
+'''
+
+# Let's say we want to find out what the user's name is. We can do that by using the input() function
+# It's quite easy once we see an exampple
+
+print("What's your name?")
+
+yourName = input()
+
+print("Your name is:")
+print(yourName)
+
+
+print("What's your age?")
+
+yourAge = input()
+
+print("Your age is:")
+print(yourAge)
+
+print("After your next birthday, you will be:")
+print(yourAge + 1)
+
+
+'''
+ACTIVITY 4
+
+'''
+
+
+
+'''
+ACTIVITY 5
+
+'''
+
+
+
+'''
+REVIEW
+'''
+#Below will be a bunch of lines of code. This is for those who learn better visually.
+
+myAge = 5
+myName = "Theo"
+myNewAge = myAge + 1
+print("hi")
+
+#print("my name is")
+#print(myName)
+print("my name is: " + myName)
+
+
+'''
+MINI-PROJECT
+
+
+'''
+# Mini-project here(maybe have them make a madlibs type thing?)
