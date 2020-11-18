c3be691bff69150ef553d6413d5e060a798347fc
Anthony Wang <ta180m@gmail.com>
2020-11-18 22:21:24 +0000

    Add group leader materials

---
+++ group_leader.md
@@ -0,0 +1,6 @@
+## For group leaders:
+
+ - Do not edit week1.py, even to fix mistakes. It'll mess things up when you try to pull the latest version on your group Repl.
+
+ - Make sure you edit the `.replit` file to point to whichever file you want to run
+ 
---
+++ week2.py
@@ -0,0 +1,18 @@
+'''
+INTRODUCTION TO
+    ______  __________  ______  _   __
+   / __ \ \/ /_  __/ / / / __ \/ | / /
+  / /_/ /\  / / / / /_/ / / / /  |/ / 
+ / ____/ / / / / / __  / /_/ / /|  /  
+/_/     /_/ /_/ /_/ /_/\____/_/ |_/   
+                                      
+WEEK 2
+
+ - Conditionals: if, else, elif
+ 
+ - Flow: for, while
+
+Don't worry if your group is a little bit behind. You'll get to the fun tic-tac-toe game soon!
+'''
+
+
