c94b2e3e8d1585950ff8c25a14ca34b235e51eec
Anthony Wang <ta180m@gmail.com>
2020-12-01 20:07:18 -0600

    Fix another bad commit

---
+++ week3.py
@@ -0,0 +1,37 @@
+'''
+INTRODUCTION TO
+    ______  __________  ______  _   __
+   / __ \ \/ /_  __/ / / / __ \/ | / /
+  / /_/ /\  / / / / /_/ / / / /  |/ / 
+ / ____/ / / / / / __  / /_/ / /|  /  
+/_/     /_/ /_/ /_/ /_/\____/_/ |_/   
+                                      
+WEEK 3
+
+ - Data types
+
+ - Functions
+
+Have students make their own files (don't copy this file)
+'''
+
+'''
+DATA TYPES
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+'''
+
+# 
+
+''''
+def my_function(fname):
+
+
+'''
+# Potential Activity: Mimic a calculator (with operations as functions, ask user for input numbers)
