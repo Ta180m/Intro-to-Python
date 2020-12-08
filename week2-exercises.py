0de3718524594f8c38fac801e9eaa70ef883dcee
Anthony Wang <ta180m@gmail.com>
2020-12-08 23:03:15 +0000

    Add exercises and week 4

---
+++ week2-exercises.py
@@ -0,0 +1,47 @@
+'''
+INTRODUCTION TO
+    ______  __________  ______  _   __
+   / __ \ \/ /_  __/ / / / __ \/ | / /
+  / /_/ /\  / / / / /_/ / / / /  |/ / 
+ / ____/ / / / / / __  / /_/ / /|  /  
+/_/     /_/ /_/ /_/ /_/\____/_/ |_/   
+                                      
+WEEK 2
+
+ - Conditionals: if, else, elif
+ 
+ - Repetition: for, while
+
+'''
+
+
+'''
+EXERCISE 1:
+
+'''
+
+
+'''
+EXERCISE 2:
+
+'''
+
+
+'''
+EXERCISE 3:
+
+'''
+
+
+'''
+EXERCISE 4:
+
+'''
+
+
+'''
+EXERCISE 5:
+
+'''
+
+
