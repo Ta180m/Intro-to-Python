27715ef991141214f8192feb7deab147a9dd8d79
Anthony Wang <ta180m@gmail.com>
2021-01-19 17:40:13 -0600

    Add advanced files

---
+++ advanced/ch1.py
@@ -0,0 +1,18 @@
+'''
+INTRODUCTION TO
+    ______  __________  ______  _   __
+   / __ \ \/ /_  __/ / / / __ \/ | / /
+  / /_/ /\  / / / / /_/ / / / /  |/ / 
+ / ____/ / / / / / __  / /_/ / /|  /  
+/_/     /_/ /_/ /_/ /_/\____/_/ |_/   
+                                      
+CHALLENGE 1: 2048
+
+Here's the link to the 2048 game made by the Ladue High School Computer Science Club: https://repl.it/join/evutnqny-ta180m
+
+The code is located in the file main.py. Play around with it and see if you can understand it! Try changing the rows and columns variables!
+
+Bonus: Help us with some of the things on the TODO list!
+'''
+
+
---
+++ advanced/ch2.py
@@ -0,0 +1,14 @@
+'''
+INTRODUCTION TO
+    ______  __________  ______  _   __
+   / __ \ \/ /_  __/ / / / __ \/ | / /
+  / /_/ /\  / / / / /_/ / / / /  |/ / 
+ / ____/ / / / / / __  / /_/ / /|  /  
+/_/     /_/ /_/ /_/ /_/\____/_/ |_/   
+                                      
+CHALLENGE 2: Python Grandmaster
+
+If you are reading this, you're probably a Python grandmaster by now. You've flown through all the basics, and the advanced stuff too! Umm need to think of a good challenge...
+'''
+
+
