b1b78ecb80c6c139464959b37e264ac348224fbe
Anthony Wang <ta180m@gmail.com>
2021-02-05 19:05:55 -0600

    Do a proper merge

--- advanced/2-dict.py
+++ advanced/2-dict.py
@@ -11,5 +11,29 @@ DICTIONARIES
 TODO
 '''
 
+# list of key value pairs
+# keys are always strings
+# keys must be unique
+
+my_dict = {
+  "name": "Bob",
+  "year": 2000,
+}
+print(my_dict)
+print("Hello", my_dict["name"])
+my_dict["year"] = 1234
+my_dict["age"] = 787
+print(my_dict)
+
 # Make sure you use activites 7-10
 #SETS
+
+# sets are lists without duplicate items
+my_set = {123,"abc",123,True}
+print(my_set)
+
+my_list = [5,1,3,3,5,5,5]
+unique_items = set(my_list)
+print(unique_items)
+
+# sets cannot contain other lists or sets
--- advanced/4-misc.py
+++ advanced/4-misc.py
@@ -13,9 +13,18 @@ TODO
 
 # Make sure you use activites 15-18
 # itertools combinations/permutations, base conversion
+# https://codingbat.com/python
 
 from itertools import product
 
 print(list(product('ab',range(3))))
 
 print(int("3A", 16))
+
+# file I/O
+
+# Installing Python on your computer
+# This gives you more flexibility for writing and testing Python code
+# Go to https://www.python.org/downloads/
+# And click on download
+# TODO
